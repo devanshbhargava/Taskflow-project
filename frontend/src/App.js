c12fdd349aba4ff482d5ee443a3e93214953cada
Devansh Bhargava <bhargavadevansh85@gmail.com>
2026-04-08 01:29:51 +0530

    remove cors from frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,12 +6,7 @@ import Login from "./login";
 import Signup from "./signup";
 import Dashboard from "./dashbord";
 import ProtectedRoute from "./protectedroutes";
-import cors from "cors";
 
-app.use(cors({
-  origin: "https://taskflow-project-cj1v-lsghrvlso-devansh6.vercel.app",
-  credentials: true
-}));
 
 function App() {
   return (
