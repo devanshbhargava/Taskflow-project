e8db072788d28354b8c5e6ac3c284713c5a3c9e1
Devansh Bhargava <bhargavadevansh85@gmail.com>
2026-04-20 02:08:33 +0530

    added mongodb connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,13 @@ import mongoose from "mongoose";
 
 const connectdb = async () => {
   try {
+    console.log("Connecting to:", process.env.MONGO_URI); // debug
+
     await mongoose.connect(process.env.MONGO_URI);
+
     console.log("✅ Database connected successfully");
   } catch (error) {
-    console.log("❌ database connection failed", error.message);
+    console.error("❌ database connection failed", error.message);
   }
 };
 
