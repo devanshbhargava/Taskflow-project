2511957319d3063091c56fe429586d9be3978295
Devansh Bhargava <bhargavadevansh85@gmail.com>
2026-04-21 00:50:21 +0530

    fix auth middleware export

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken"; 
 
-module.exports = (req, res, next) => {
+const auth = (req, res, next) => {
   const token = req.header("Authorization")?.split(" ")[1];
 
   if (!token) return res.status(401).json({ message: "No token" });
