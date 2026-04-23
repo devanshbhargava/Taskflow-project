42c74c1bbf9121cf8d116460180412c83f3f8951
Devansh Bhargava <bhargavadevansh85@gmail.com>
2026-04-24 03:14:35 +0530

    fixed jwt secret issue

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -6,7 +6,7 @@ const auth = (req, res, next) => {
   if (!token) return res.status(401).json({ message: "No token" });
 
   try {
-    const decoded = jwt.verify(token, "secretkey");
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);;
     req.user = decoded;
     next();
   } catch {
